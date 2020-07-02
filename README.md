f55a5c4c6e725e0b4b7e8d59b26ea4677d43a518
grimmgr <ggrimm33@gmail.com>
2020-07-01 17:41:44 -0700

    created password generator

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # homework-3
 Password Generator
+
+- created arrays with possible characters to include in password.
+- used Prompt to get and store user's desired password length.
+- used confirms and push.apply to gather user's desired types of characters into a single array called charArray.
+- used a for loop with the password length to push random characters from charArray into an array called newPwArray.
+- returned newPwArray joined into a string.
---
+++ script.js
@@ -0,0 +1,63 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+const lowerCaseLetter = ['a', 'b','c' ,'d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const upperCaseLetter = lowerCaseLetter.map(let => let.toUpperCase());
+const number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+const specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '?'];
+
+
+
+
+function generatePassword() {
+  let charArray = [];
+  let newPwArray = [];
+  let pwLength = prompt('How many characters do you want in your password?  Please enter a number between 8 and 128:');
+  if (pwLength === null) {
+    return '';
+  }
+  while (pwLength < 8 || pwLength > 128) {
+    pwLength = prompt('You must choose a password length of at least 8 and no more than 128:');
+    if (pwLength === null) {
+      return '';
+    }
+  }
+  
+  while (charArray.length === 0 ) {
+    if (confirm('Please choose at least one of the following types of characters to use in you password:')) {
+      if (confirm('Do you want lowercase letters in you password?')) {
+        charArray.push.apply(charArray, lowerCaseLetter);
+      }
+      if (confirm('Do you want uppercase letters in you password?')) {
+        charArray.push.apply(charArray, upperCaseLetter);
+      }
+      if (confirm('Do you want numbers in you password?')) {
+        charArray.push.apply(charArray, number);
+      }
+      if (confirm('Do you want special characters in you password?')) {
+        charArray.push.apply(charArray, specialChar);
+      } 
+    } else {
+      return '';
+    }
+  }
+  
+  for (let i = 0; i < pwLength; i++) {
+    let randomIndex = Math.floor(Math.random() * charArray.length);
+    newPwArray.push(charArray[randomIndex]);
+  }
+  return newPwArray.join('');
+}
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+  
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
